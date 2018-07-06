f8930840a224b6770273c948a38df125b520582c
测试 <2454576922@qq.com>
2018-07-06 11:14:27 +0800

    陈静

---
+++ bdqn.T115/MvcApplication1/Controllers/ChosultationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bdqn.webApp.Controllers
+{
+    public class ChosultationController : Controller
+    {
+        //
+        // GET: /Chosultation/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ bdqn.T115/MvcApplication1/Controllers/StudentarchivesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bdqn.webApp.Controllers
+{
+    public class StudentarchivesController : Controller
+    {
+        //
+        // GET: /Studentarchives/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
